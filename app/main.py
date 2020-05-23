a204fe17bba84dab07cbe22c0cf38414e5984a16
viktoriyazhumaeva <viktoriya2598@mail.ru>
2020-05-23 22:05:01 +0300

    11

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mebel.db'
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Логин', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить пароль')
+    submit = SubmitField('Войти')
+
